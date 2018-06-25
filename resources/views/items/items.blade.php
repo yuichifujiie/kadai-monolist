3ed05618ed0144c86b7166e10b388aea51e61fa6
EC2 Default User <ec2-user@ip-172-31-43-12.us-east-2.compute.internal>
2018-06-25 06:14:53 +0000

    want

---
+++ resources/views/items/items.blade.php
@@ -0,0 +1,25 @@
+@if ($items)
+    <div class="row">
+        @foreach ($items as $item)
+            <div class="item">
+                <div class="col-md-3 col-sm-4 col-xs-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading text-center">
+                            <img src="{{ $item->image_url }}" alt="">
+                        </div>
+                        <div class="panel-body">
+                            @if ($item->id)
+                                <p class="item-title"><a href="{{ route('items.show', $item->id) }}">{{ $item->name }}</a></p>
+                            @else
+                                <p class="item-title">{{ $item->name }}</p>
+                            @endif
+                            <div class="buttons text-center">
+                                @include('items.want_button', ['item' => $item])
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+@endif
