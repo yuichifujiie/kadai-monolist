ee5970222aef3ff63bffe5a7b3f37a896377055f
EC2 Default User <ec2-user@ip-172-31-43-12.us-east-2.compute.internal>
2018-06-26 07:11:40 +0000

    ranking

--- routes/web.php
+++ routes/web.php
@@ -24,17 +24,20 @@ Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
+Route::get('ranking/want', 'RankingController@want')->name('ranking.want');
+Route::get('ranking/have', 'RankingController@have')->name('ranking.have');
+
 Route::group(['middleware' => ['auth']], function () {
     Route::resource('items', 'ItemsController', ['only' => ['create']]);
 });
 
 Route::group(['middleware' => ['auth']], function () {
     Route::resource('items', 'ItemsController', ['only' => ['create', 'show']]);
-     Route::post('want', 'ItemUserController@want')->name('item_user.want');
+    Route::post('want', 'ItemUserController@want')->name('item_user.want');
     Route::post('have', 'ItemUserController@have')->name('item_user.have');
+    Route::post('want', 'ItemUserController@want')->name('item_user.want');
     Route::delete('want', 'ItemUserController@dont_want')->name('item_user.dont_want');
     Route::delete('have', 'ItemUserController@dont_have')->name('item_user.dont_have');
+    Route::delete('want', 'ItemUserController@dont_want')->name('item_user.dont_want');
     Route::resource('users', 'UsersController', ['only' => ['show']]);
 });
-
-   
